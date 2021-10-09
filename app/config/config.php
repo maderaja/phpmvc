a8c2bca064acfe1c829ddad3dfc65f12853e6910
RajaMahendra <maderajam07@gmail.com>
2021-10-09 11:16:55 +0800

    perbaiki baseurl

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,11 @@
 <?php
 
-define('BASEURL', 'http://localhost:8080/phpmvc/public');
+define('BASEURL', 'http://localhost/phpmvc/public');
 
 // DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'phpmvc');
+
+//wkwkwk coba apakah ini folder yg ke commit
